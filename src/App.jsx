829735852d05b3275654d9049411bd39a9df703b
Braian <braianalexisamaya16@gmail.com>
2026-09-05 21:06:22 -0300

    Funciona Ficha de Turno pero falta el css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
 import { useState } from 'react'
+import FichaDeTurno from './components/fichaDeTurno/FichaDeTurno'
 
 function App() {
-  //const [count, setCount] = useState(0)
 
   return (
-    <>
+    <main>
+
+      <FichaDeTurno cargando={true} />
       
-    </>
+      <FichaDeTurno error={true} />
+      
+      <FichaDeTurno turno={null} />
+      
+      <FichaDeTurno turno={{
+        paciente: 'Ceferino Ruiz',
+        hora: '09:30',
+        urgente: true,
+        observaciones: 'Traer estudios previos',
+        sesiones: 4
+      }} />
+      
+      <FichaDeTurno turno={{
+        paciente: 'Ana Gómez',
+        hora: '10:00',
+        urgente: false,
+        observaciones: null,
+        sesiones: 0
+      }} />
+    </main>
   )
 }
 
